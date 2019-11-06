ca1f81628cc10d8a568be3b6201ac10c7bf3b44f
Alexandre Florindo <alexandreflorindo@MBPdeAlexandre.lan>
2019-11-06 18:14:34 +0000

    Final

--- scripts/music.js
+++ scripts/music.js
@@ -6,13 +6,17 @@ let audio1 = document.getElementById("audio1");
 let buttonPlay = document.getElementById("btn");
 let buttonStop = document.getElementById("btn1");
 let buttonStopGame = document.getElementById("btnStop");
+let buttonStopGame1 = document.getElementById("btnStop1");
 
 let playMusic = () => audio.play();
 let stopMusic = () => audio.pause();
 let stopMusicInGame = () => audio.pause();
+let stopMusicInGame1 = () => audio.pause();
 
 buttonPlay.addEventListener("click", playMusic);
 
 buttonStop.addEventListener("click", stopMusic);
 
 buttonStopGame.addEventListener("click", stopMusicInGame);
+
+buttonStopGame1.addEventListener("click", stopMusicInGame1);
